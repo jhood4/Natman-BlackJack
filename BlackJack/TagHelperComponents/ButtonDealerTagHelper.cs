1aadcdda1c19582713e542799ff16eefafada0a4
jhood44 <jhood4@gmail.com>
2021-12-11 03:27:06 -0500

    deal button fixed

--- BlackJack/TagHelperComponents/ButtonDealerTagHelper.cs
+++ BlackJack/TagHelperComponents/ButtonDealerTagHelper.cs
@@ -13,27 +13,34 @@ namespace BlackJack.TagHelperComponents
     [HtmlTargetElement("showdealerbutton")]
     public class ButtonDealerTagHelper : TagHelper
     {
+        private ISession session { get; set; }
+        public bool NeedsDeal { get; set; }
+        public ButtonDealerTagHelper(IHttpContextAccessor accessor)
+        {
+            session = accessor.HttpContext.Session;
+            NeedsDeal = Convert.ToBoolean(session.GetInt32("deal"));
+        }
+
         public override void Process(TagHelperContext context,
         TagHelperOutput output)
         {
-        output.TagName = "form";
-  
-        output.Attributes.SetAttribute("class", "col");
-        output.TagMode = TagMode.StartTagAndEndTag;
-        var content = "";
+            output.TagName = "form";
+
+            output.Attributes.SetAttribute("class", "col-2 offset-3");
+            output.TagMode = TagMode.StartTagAndEndTag;
+            var content = "";
 
 
-        
+
             if (NeedsDeal)
             {
-                content += $"< button type="submit" class="btn btn-primary">Deal</button>";
+                content += $"< button type='submit' class='btn btn-primary'>Deal</button>";
             }
             else
             {
-                <button type="submit" class="btn btn-primary" disabled>Deal</button>
+                content += "<button type='submit' class='btn btn-primary' disabled>Deal</button>";
             }
-
-        
+            output.Content.SetHtmlContent(content);
         }
     }
 }
