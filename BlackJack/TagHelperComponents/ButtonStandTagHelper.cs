cd450a08eb135943986855e76b85ff0b42afe2c7
Dayvon Rose <35536031+DayvonRose@users.noreply.github.com>
2021-12-11 23:24:28 -0500

    Test

---
+++ BlackJack/TagHelperComponents/ButtonStandTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using BlackJack.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack.TagHelperComponents
+{
+    [HtmlTargetElement("showstandbutton")]
+    public class ButtonStandTagHelper : TagHelper
+    {
+        private ISession session { get; set; }
+        public bool NeedsDeal { get; set; }
+        public ButtonStandTagHelper(IHttpContextAccessor accessor)
+        {
+            session = accessor.HttpContext.Session;
+            NeedsDeal = Convert.ToBoolean(session.GetInt32("stand"));
+        }
+
+        public override void Process(TagHelperContext context,
+        TagHelperOutput output)
+        {
+            output.TagName = "form";
+
+            output.Attributes.SetAttribute("class", "col-2");
+            output.TagMode = TagMode.StartTagAndEndTag;
+            var content = "<form asp-action='Stand' method='post' class='col'>";
+
+
+
+            if (!NeedsDeal)
+            {
+                content += $"<button type='submit' class='btn btn-primary'>Stand</button>";
+            }
+            else
+            {
+                content += "<button type='submit' class='btn btn-primary' disabled>Stand</button>";
+            }
+            content += "</form>";
+            output.Content.SetHtmlContent(content);
+        }
+    }
+}
